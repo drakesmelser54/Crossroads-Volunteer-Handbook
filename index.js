f4eaeac79e4fe029df8f68f51488f98472ed1360
Drake Smelser <drakesmelser54@gmail.com>
2021-07-20 21:53:25 -0400

    add Our Story Page

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const router = new Navigo(window.location.origin);
 
 router
   .on({
-    ":page": params => render(state[capitalize(params.page)]),
-    "/": () => render(state.Home)
+    ":page": (params) => render(state[capitalize(params.page)]),
+    "/": () => render(state.Home),
   })
   .resolve();
 
@@ -30,68 +30,69 @@ function render(st = state.Home) {
 //---------------------------------Event Listener Bundler-------------------------------------------
 function eventListenerBundler(st) {
   listenForLogin(st);
-    listenForSignup(st);
+  listenForSignup(st);
 }
 
 //----------signup-form-------------/
 function listenForSignup(st) {
   if (st.view === "Signup") {
-    document.querySelector("#signup-form").addEventListener("submit", event => {
-      event.preventDefault();
-      //convert html elements to Array
-      let inputList = Array.from(event.target.elements);
-      //remove submit button so it's not included
-      inputList.pop();
-      const inputs = inputList.map(input => input.value);
-      let email = inputs[0];
-      let password = inputs[1];
-      //create user in firebase
-      auth.createUserWithEmailAndPassword(email, password).then(response => {
-        //add user to state and database
-        addUserToStateAndDB(
-          email,
-          password
-        );
-      render(state.Welcome);
+    document
+      .querySelector("#signup-form")
+      .addEventListener("submit", (event) => {
+        event.preventDefault();
+        //convert html elements to Array
+        let inputList = Array.from(event.target.elements);
+        //remove submit button so it's not included
+        inputList.pop();
+        const inputs = inputList.map((input) => input.value);
+        let email = inputs[0];
+        let password = inputs[1];
+        //create user in firebase
+        auth
+          .createUserWithEmailAndPassword(email, password)
+          .then((response) => {
+            //add user to state and database
+            addUserToStateAndDB(email, password);
+            render(state.Welcome);
+          });
+        router.navigate("/Welcome");
       });
-      router.navigate("/Welcome")
-    });
   }
 }
 
 //--------add user to state and db-----------/
-function addUserToStateAndDB(
-  email,
-  password
-) {
+function addUserToStateAndDB(email, password) {
   state.User.email = email;
   state.User.loggedIn = true;
 
   db.collection("users").add({
     email: email,
     signedIn: true,
-    loggedIn: true
+    loggedIn: true,
   });
 }
-
 
 //-----------------------------------------login----------------------------------------------------
 function listenForLogin(st) {
   if (st.view === "Login") {
-    document.querySelector("#login-form").addEventListener("submit", event => {
-      event.preventDefault();
-      //convert html elements to Array
-      let inputList = Array.from(event.target.elements);
-      //remove the login button so it's not included
-      inputList.pop();
-      const inputs = inputList.map(input => input.value);
-      let email = inputs[0];
-      let password = inputs[1];
-      auth.signInWithEmailAndPassword(email, password).then(() => {
-        getUserFromDb(email)
-          .then(() => render(state.Welcome), router.navigate("/Welcome"))
+    document
+      .querySelector("#login-form")
+      .addEventListener("submit", (event) => {
+        event.preventDefault();
+        //convert html elements to Array
+        let inputList = Array.from(event.target.elements);
+        //remove the login button so it's not included
+        inputList.pop();
+        const inputs = inputList.map((input) => input.value);
+        let email = inputs[0];
+        let password = inputs[1];
+        auth.signInWithEmailAndPassword(email, password).then(() => {
+          getUserFromDb(email).then(
+            () => render(state.Welcome),
+            router.navigate("/Welcome")
+          );
+        });
       });
-    });
   }
 }
 
@@ -100,8 +101,8 @@ function getUserFromDb(email) {
   return db
     .collection("users")
     .get()
-    .then(snapshot =>
-      snapshot.docs.forEach(doc => {
+    .then((snapshot) =>
+      snapshot.docs.forEach((doc) => {
         if (email === doc.data().email) {
           let id = doc.id;
           db.collection("users")
@@ -114,3 +115,6 @@ function getUserFromDb(email) {
       })
     );
 }
+
+
+
