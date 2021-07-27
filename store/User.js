b956f75aa20bfb80700ce74d41d8578572063811
Drake Smelser <drakesmelser54@gmail.com>
2021-07-27 11:41:55 -0400

    fix store merge conflicts

--- store/User.js
+++ store/User.js
@@ -2,20 +2,6 @@ export default {
   email: "",
   loggedIn: false,
   lessons: {
-<<<<<<< HEAD
     story: false
-=======
-    story: false,
-    mission: false,
-    what: false,
-    who: false,
-    volunteering: false,
-    policy: false,
-    domestic: false,
-
-
-
-
->>>>>>> 45f9bd7
   }
 };
