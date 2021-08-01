9091579cead43c49023f979648106babcb7192e8
Drake Smelser <drakesmelser54@gmail.com>
2021-08-01 01:59:36 -0400

    finish what do we do lesson

--- components/views/Do.js
+++ components/views/Do.js
@@ -6,8 +6,48 @@ export default () => `
     </a>
     <h1 id="history-header">What We Do</h1>
   </div>
+  <div class="do-container">
   <h2>What Does Crossroads Offer?</h2>
-  <h2>Domestic Violence Program & Shelter</h2>
+  <img src="https://raw.githubusercontent.com/drakesmelser54/Crossroads-Volunteer-Handbook/master/images/hands.jpg.jpg" alt="hands" height="200" />
+  <ul>
+    <li> Hundreds of affordable housing units for families and individuals
+    </li>
+    <li>Comprehensive case management</li>
+    <li>Education and employment services</li>
+    <li>24-hour/7 day a week emergency services</li>
+  </ul>
+  <h4>Our goal is to end homelessness in Rhode Island. This means making the experience of homelessness as temporary as possible by helping families and individuals secure stable homes quickly.</h4>
+  <h2 id="special-do">Domestic Violence Program & Shelter</h2>
+  <img src="https://raw.githubusercontent.com/drakesmelser54/Crossroads-Volunteer-Handbook/master/images/friends.jpg%20(1).jpg" alt="friends" height="200" />
+  <h4>The Domestic Violence Program is primarily an emergency shelter program with a focus on safety and housing. The shelter provides safety for domestic violence survivors and their children in a confidential location separate from other Crossroads properties. The domestic violence program also includes transitional housing and a 24-hour helpline. Clients will have access to Crossroads Rhode Island’s range of services, including basic needs assistance, education and employment services, and intensive case management, while they work to find a secure and stable home.<br></br>
+  The women’s shelter is the oldest and largest domestic violence shelter in Rhode Island. We are affiliated with the RI Coalition Against Domestic Violence (RIADV). The services we offer are:</h4>
+  <ul>
+    <li>Emergency Shelter</li>
+    <li>24/7 Confidential Helpline</li>
+    <li>Children’s Enrichment Program</li>
+    <li>Community Support Group</li>
+    <li>Transitional Housing</li>
+    <li>All services offered by Crossroads RI </li>
+  </ul>
+</div>
+<h2 id="programs-header">Domestic Violence Programs & Services</h2>
+<img src="https://raw.githubusercontent.com/drakesmelser54/Crossroads-Volunteer-Handbook/master/images/house%20(1).jpg" alt="house sketch" height="300"/>
+<div class="do-column-container">
+  <div class="do-columns">
+    <h3>Residential Program</h3>
+    <h4>The Domestic Violence Program’s facility allows each family its own room as well as space for group meetings. Single women may share a room. When residents enter the program, they receive the basic needs of shelter, food, clothing and safety. In addition, a case manager assists each woman in working on her goals. These may include finding an apartment, medical and legal concerns, establishing a means of income, returning to school, and getting or keeping a job. Clients attend groups aimed at helping them express their feelings in a healthy way and heal from their experiences. The Family Advocate works with each child and meets with the mother to identify needs and set goals. Residents can stay with us from one to three months. </h4>
+  </div>
+  <div class="do-columns">
+    <h3>Transition Program</h3>
+    <h4>The goal of the Transitional Program is to offer services and support to women while they progress to more independent lives. Residents seeking to re-enter the workforce, upgrade their current situation, or further their education, receive help with career development plans, educational options, writing resumes, completing financial aid forms for college/continuing education, and referrals to outside vocational and educational programs.<br></br>
+    Clients learn and actively practice the skills necessary for independent living in their community, including finding housing and running a household, accessing medical or mental health services, finding help in a crisis or emergency, getting and holding a job, completing education, and improving parenting skills. The program continues to help victimized women to develop their sense of self-worth and self-confidence while helping them achieve their full potential by gradually widening their relational circles with families, peers and community resources.</h4>
+  </div>
+  <div class="do-columns">
+    <h3>Child Enrichment Program</h3>
+    <h4>The goal of the Children's Enrichment Program is to create a safe, supportive learning environment for residential children to enhance their social, emotional, cognitive, physical, and linguistic development. Research has demonstrated that it is extremely important to effectively and therapeutically address the experience of interpersonal family violence in children. It has established that it is necessary to help children understand and cope with their emotional responses to the violence, while promoting their acquisition of positive behavior patterns. To address these issues, the Family Advocate and volunteers focus on developing social skills, coping strategies, expressing emotions, conflict resolution, self-esteem, and healthy relationships.
+    </h4>
+  </div>
+</div>
   <a id="next-button" href="/Who" data-navigo>Next Lesson</a>
 </div>
 `;
