0a3eb8e96b81da4b017802ddd7f8673202d4f20b
Drake Smelser <drakesmelser54@gmail.com>
2021-07-31 22:40:56 -0400

    finish story page with pics

--- components/views/Story.js
+++ components/views/Story.js
@@ -1,31 +1,34 @@
 export default () => `
 <div class="story-jumbotron">
-  <div class="upper-story-container">
-    <div id="previous-lesson">
-      <i class="fas fa-long-arrow-alt-left"></i>
-      <p>&nbsp&nbspPrevious Lesson</p>
-    </div>
-    <h2 id="history-header">A Brief History of Crossroads</h2>
+  <div class="upper-container">
+      <a href="/Handbook" data-navigo>
+        <p>&nbsp&nbspBack to Volunteer Handbook</p>
+      </a>
+    <h1 id="history-header">A Brief History of Crossroads</h1>
   </div>
   <h2 id="story-headers">1894</h2>
-  </h2>
-  <img class="story-images" src="https://d1b4xsuwkkd7wf.cloudfront.net/banners/_1200x630_crop_center-center_82_none/img64.jpg?mtime=1572447528" alt="travellers aid photo" />
-  </div>
+  <img class="story-photos"
+    src="https://raw.githubusercontent.com/drakesmelser54/Crossroads-Volunteer-Handbook/master/images/travellers-aid.jpg" alt="travellers-aid photo" />
   <div class="stories">
     <p>We were founded as Travelers Aid of Rhode Island The goal was to assist people who came to Providence in search of work, many of whom were young women. Since then, we have evolved to meet the needs of the most vulnerable people in our state.
     </p>
   </div>
   <h2 id="story-headers">2004</h2>
+  <img class="story-photos"
+  src="https://d1b4xsuwkkd7wf.cloudfront.net/banners/_1200x630_crop_center-center_82_none/building.jpg?mtime=1574091680" alt="160 Broad St. photo" />
   <div class="stories">
     <p>In 2004, in order to reflect all the changes that we had made, we changed our name to Crossroads Rhode Island and moved from downtown Providence into the former YMCA building at 160 Broad Street.
     Today we are the largest homeless services organization in Rhode Island. We have become much more than a place to sleep. In addition to shelter and basic needs,
     <p>
   </div>
   <h2 id="story-headers">2016</h2>
+  <img class="story-photos"
+  src="https://d1b4xsuwkkd7wf.cloudfront.net/images/Photoshoot/_halfSize/women-in-shelter.jpg"
+  alt="women of crossroads" />
   <div class="stories">
     <p>On July 1, 2016, the Women’s Center of RI merged into the Domestic Violence Program of Crossroads RI. As domestic violence is one of the leading causes of homelessness for women and children, both organizations strive to empower vulnerable individuals and families to overcome homelessness and live, safe, self-sufficient lives free of violence. The merger has enabled us to better address the connection between domestic violence and homelessness.
     </p>
   </div>
-  <a id="next-button" href="/Handbook" data-navigo>Next Lesson</a>
+  <a id="next-button" href="/Mission" data-navigo>Next Lesson: Mission, Vision, & Values</a>
 </div>
 `;
