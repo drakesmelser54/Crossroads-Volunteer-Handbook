d87d84426285f84505844768ddf57baf2d1667e7
Drake Smelser <drakesmelser54@gmail.com>
2021-08-09 20:02:29 -0400

    finish volunteering page

--- components/views/Volunteering.js
+++ components/views/Volunteering.js
@@ -1,29 +1,159 @@
 export default () => `
 <div class="story-jumbotron">
-  <div class="upper-story-container">
-    <div id="previous-lesson">
-      <i class="fas fa-long-arrow-alt-left"></i>
-      <p>&nbsp&nbspPrevious Lesson</p>
+  <div class="upper-container">
+    <a href="Do" data-navigo">
+    <p>&nbsp&nbspPrevious Lesson</p>
+    </a>
+    <h1 id="history-header">Volunteering</h1>
+  </div>
+  <h2 id="opportunity-header">Volunteer Opportunities</h2>
+  <div class="upper-volunteering-container">
+    <div class="volunteering-icon-container">
+      <img src="https://github.com/drakesmelser54/Crossroads-Volunteer-Handbook/blob/master/images/24:7-icon.png?raw=true" alt="clock" / height="150">
+      <h3>Hotline/Front Desk</h3>
     </div>
-    <h2 id="history-header">A Brief History of Crossroads</h2>
+    <div class="volunteering-icon-container">
+      <img src="https://github.com/drakesmelser54/Crossroads-Volunteer-Handbook/blob/master/images/children-reading-icon.png?raw=true" alt="children reading" / height="150">
+      <h3>Children's Enrichment Program</h3>
+    </div>
+    <div class="volunteering-icon-container">
+      <img src="https://github.com/drakesmelser54/Crossroads-Volunteer-Handbook/blob/master/images/earth-icon.png?raw=true" alt="Earth" / height="150">
+      <h3>On-call Interpreter</h3>
+    </div>
+    <div class="volunteering-icon-container">
+      <img src="https://github.com/drakesmelser54/Crossroads-Volunteer-Handbook/blob/master/images/cleaning-icon.png?raw=true" alt="Mop bucket" / height="150">
+      <h3>House-keeping/Maintenance</h3>
+    </div>
+    <div class="volunteering-icon-container">
+      <img src="https://github.com/drakesmelser54/Crossroads-Volunteer-Handbook/blob/master/images/donations-icon.png?raw=true" alt="Donations" / height="150">
+      <h3>Donation Room</h3>
+    </div>
   </div>
-  <h2 id="story-headers">1894</h2>
-  </h2>
-  <div class="stories">
-    <p>We were founded as Travelers Aid of Rhode Island The goal was to assist people who came to Providence in search of work, many of whom were young women. Since then, we have evolved to meet the needs of the most vulnerable people in our state.
-    </p>
+  <div class="confidentiality-statements">
+    <h2 id="volunteering-headers">Crossroads Confidentiality</h2>
+    <p>Crossroads Rhode Island is a multi-dimensional agency providing a wide range of services to its’ clients. It is important that all staff and volunteers are aware that they are privy to private information about our clientele and hold themselves to a high standard of confidentiality and ethics.<br></br>
+    In order to foster and maintain the trust and confidence of our clients, it is the policy of Crossroads Rhode Island that all staff and volunteers make an affirmative effort to be aware of and adhere to all ethical and professional codes of conduct and confidentiality practices applicable to the professional disciplines represented by the agency. This applies to both during and after employment or volunteer service.</p>
   </div>
-  <h2 id="story-headers">2004</h2>
-  <div class="stories">
-    <p>In 2004, in order to reflect all the changes that we had made, we changed our name to Crossroads Rhode Island and moved from downtown Providence into the former YMCA building at 160 Broad Street.
-    Today we are the largest homeless services organization in Rhode Island. We have become much more than a place to sleep. In addition to shelter and basic needs,
-    <p>
+  <img class="volunteering-icons" src="https://github.com/drakesmelser54/Crossroads-Volunteer-Handbook/blob/master/images/confidentiality%20(1).jpg?raw=true" height="150">
+  <div id="dvp-confidentiality" class="confidentiality-statements">
+    <h2 id="volunteering-headers">DVP Confidentiality</h2>
+    <ul>
+      <li>Please do not discuss a person receiving services from the DVP’s situation with anyone that is not employed or an active volunteer for the DVP. This includes written communication, phone conversations and internal and external e-mail.</li>
+      <li>Do not discuss a person’s situation while s/he is receiving services from the DVP in common areas: reception area, hallways, copy room, stairwells, etc. during the warmer months, please be aware of open windows.</li>
+      <li>Please do not discuss confidential information in the presence of children.</li>
+      <li> Please be aware when you are handling confidential paperwork that you:</li>
+      <ul>
+        <li>Keep the office door closed when discussing anyone’s personal information.</li>
+        <li>Make sure all paperwork of a confidential nature is put away before you leave it for any reason no matter the length of time.</li>
+      </ul>
+      <li>In public, when you see a current or former client of DVP that you recognize:</li>
+      <ul>
+        <li>Do not initiate contact. People receiving services from us know that we will not initiate contact with them to protect their confidentiality.</li>
+        <li>If the person initiates contact with you, you are welcome to respond, but do not discuss anything of a confidential nature.</li>
+        <li>Do not say in front of others that you may have met the person before or that you know anything about the person that you have learned through their receiving services from the Center.
+        </li>
+        <li>Do not treat the person differently from anyone else in the same situation.</li>
+      </ul>
+      <li> Home and cell phone numbers of DVP employees and volunteers are confidential. They are listed at the front desk in the census book for interoffice use only.</li>
+    </ul>
   </div>
-  <h2 id="story-headers">2016</h2>
-  <div class="stories">
-    <p>On July 1, 2016, the Women’s Center of RI merged into the Domestic Violence Program of Crossroads RI. As domestic violence is one of the leading causes of homelessness for women and children, both organizations strive to empower vulnerable individuals and families to overcome homelessness and live, safe, self-sufficient lives free of violence. The merger has enabled us to better address the connection between domestic violence and homelessness.
-    </p>
+  <div class="ethics">
+    <div class="ethics-statements">
+      <h2 id="ethics-headers">Crossroads Ethics</h2>
+      <p>Crossroads RI strives to maintain high standards of competence in all areas. Each person, employee, volunteer, client is to be respected.<br></br>
+      No employee, volunteer, or any other person under the auspices of Crossroads Rhode Island will:</p>
+      <ul>
+        <li>Enter into personal relationship with a client until that person has ceased to be a client for a period of twelve (12) months. Cessation of client status is considered the date the case file is closed or the date upon which a person ceases to visit the agency on a regular basis for any reason.</li>
+        <li>Give or receive any special favor(s) or any special attention to or from clients. This includes the giving/receiving of gifts/cash/loans to or from clients.</li>
+        <li>Accommodate clients in their home, give out their home address, email address, or phone number or that of a co-worker, or provide any personal information.</li>
+        <li>Should an issue arise which is not covered by the above, or under circumstances where a staff person or volunteer is not sure of the appropriate response, the issue in question should be brought to the attention of the Agency President. Should a staff member or volunteer find that it is not practical to refrain from acting on the issue in question, then s/he should respond in the manner felt to be most appropriate and should report the matter to the Agency President through the Unit Director within twenty-four hours.
+        </li>
+      </ul>
+    </div>
+    <img class="volunteering-icons" src="https://github.com/drakesmelser54/Crossroads-Volunteer-Handbook/blob/master/images/DVP-Logo.png?raw=true" height="200" />
+    <div class="ethics-statements">
+    <h2 id="ethics-headers">Volunteers Code of Conduct</h2>
+      <ul>
+        <li>Take your commitment to our organization to heart, performing your duties to the best of your ability.</li>
+        <li>Honor confidentiality.</li>
+        <li>Respect the mission and goals of the Domestic Violence Program and Crossroads RI. </li>
+        <li>Deal with conflicts or difficulties in an appropriate manner as outlined in this manual.</li>
+        <li>Respect the property of the Domestic Violence Program and Crossroads RI. </li>
+        <li>Do not accept valuable gifts or money from clients.</li>
+        <li>Be courteous, friendly and cooperative. </li>
+        <li>Offer constructive feedback about our organization in an appropriate manner.  </li>
+        <li>Be willing to learn and take part in orientation and training sessions. </li>
+        <li>Follow through on commitments and advise your supervisor or the volunteer coordinator if you are unable to work as scheduled. </li>
+        <li>Demonstrate respect for the direction and decisions of your supervisor(s).  </li>
+        <li>Treat co-workers (paid and unpaid), clients and members of the public fairly and without discrimination. </li>
+      </ul>
+    </div>
   </div>
-  <a id="next-button" href="/Handbook" data-navigo>Next Lesson</a>
+  <div class="logistics-container">
+    <div class="logistics-statements">
+      <h2 id="volunteering-headers">DVP Logistics</h2>
+      <h3>House of Operations</h3>
+      <p>The Domestic Violence Program has a 24-hour domestic violence hotline and shelter for people in crisis or in need of support. Typically, our administrative as well as community-based services operate during regular business hours; Monday-Friday from 8:30 AM – 5:00 PM.Volunteers who serve in the building do so from 9:00 AM – 8:30 PM. If you need to leave a message for a staff person, please do so between 9:00 AM – 8:30 PM.If you must leave a staff person a message between 8:30 PM and 9:00 AM, do not call the hotline and wake the overnight residential advocate. You can call their extension and leave a voicemail.</p>
+      <h3>Dress Code</h3>
+      <p>It is the policy of Crossroads Rhode Island to allow employees, volunteers, and interns’ freedom in their choice of attire as long as they meet the guidelines listed below. We believe that employees/volunteers/interns can freely choose apparel that demonstrates individuality, as well as maintain the standards of appropriateness in a manner befitting the professionalism of their positions.</p>
+      <img class="volunteering-icons" src="https://www.nicepng.com/png/detail/194-1943542_logistics-logistics-icon.png" alt="logistics icons" height="200" />
+      <div class="mailing-container">
+        <h3>Mailing Address</h3>
+        <p>The DVP’s mailing address is: P. O. BOX 5754, Providence, RI 02906.
+        Both our location and our street address (the street name and number) are kept confidential for the safety of the house. Our P. O. Box address can be given out and is listed above. If you are asked for the street address or for directions, please find out why the caller needs this information.<br></br>
+        The street address can only be given under the following circumstances: </p>
+        <ul>
+          <li>The caller is coming for a meeting with a staff member. VERIFY THE VISIT WITH THE STAFF MEMBER OR CHECK THE EXPECTED VISITOR’S LOG BEFORE GIVING OUT ANY INFORMATION.</li>
+          <li>The caller is making a delivery. If the delivery is not written down on the expected visitor’s log, ask who placed the order and check with that staff person before giving out any information.</li>
+        </ul>
+        <h3>Checking In/Tracking Hours</h3>
+        <ul>
+          <li>Checking In:</li>
+          <ul>
+            <li>The volunteer coordinator will check-in weekly via phone/in-person/email to de-brief about your week.</li>
+            <li>Monthly de-briefing meetings will happen if necessary, especially for those who work on the hotline. </li>
+          </ul>
+          <li>Tracking Hours:</li>
+         <ul>
+            <li>All Volunteers must sign in and out of the Volunteer Binder</li>
+            <li>Hours Letter can be provided by the Volunteer Coordinator</li>
+          </ul>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="emergency-container">
+    <img class="volunteering-icons" src="https://github.com/drakesmelser54/Crossroads-Volunteer-Handbook/blob/master/images/emergency-icon.jpg?raw=true" alt="fire alarm" height="200" />
+    <h2 id="emergency-headers">In Case of Emergency</h2>
+    <div class="emergency-statements">
+      <h3 id="emergency-headers">Fire</h3>
+      <ul>
+        <li>In case of a fire, emergency, other event requiring evacuation</li>
+        <li>Staff/Volunteer (at the Front Desk) should take sign-out board, the census book, and the keys to front door.</li>
+        <li>Conduct a head-count and check any discrepancies against census book and sign –out board</li>
+        <li>If you discover fire before sprinkler or alarm goes off, pull the fire alarm closet to you. </li>
+        <li>There are fire extinguishers located on each floor of the building.</li>
+        <li>Remember: there are no exits in the courtyard!</li>
+      </ul>
+      <h3 id="emergency-headers">Illness/Injury/Medical Emergency</h3>
+      <ul>
+        <li>Staff person will determine if ambulance should be called. First aid kits are located at the front desk and basement meeting space.</li>
+        <li>If situation needs immediate attention, call 911.</li>
+      </ul>
+      <h3 id="emergency-headers">Security Alarm</h3>
+        <ul>
+          <li>Check cameras to determine if there is reasonable explanation. Notify staff in building.</li>
+          <li>All outside doors and windows are alarmed.</li>
+          <li>ADT will call. If explanation is found, give password. If no obvious explanation is found, do not answer or do not give password.</li>
+          <li>If intrusion is suspected, call 911 immediately.</li>
+        </ul>
+      <h3 id="emergency-headers">Unexpected Visitor/Abuser</h3>
+      <ul>
+          <li>If the visitor is not expected, is not on the Expected Visitor’s Log, use the same protocol as above for screening. DO NOT LET IN any visitor who is not expected without clearance from a staff person. Instead, ask who the person is, who they would like to see etc. without confirming or denying any residents participation in our program or confirming the name of our agency. The visitor may just be at the wrong location.</li>
+          <li>If you sense that the individual is trying to come in or gain information about a resident you must inform the individual that you cannot confirm or deny that that person is or is not here and ask that the visitor leave. It would also be helpful to write up a description of the individual and the vehicle they were driving for other staff/volunteers sitting at the front desk. Immediately call/notify staff if an incident like this occurs.</li>
+      </ul>
+    </div>
+  </div>
+  <a id="next-button" href="/Policy" data-navigo>Next Lesson: Policy</a>
 </div>
 `;
